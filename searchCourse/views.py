255d3019a3ddd6650d2d5d6e71120cfa14a69e45
Christian Lo <sporewoh@gmail.com>
2018-07-26 22:17:54 -0600

    Added detail view to courseSearch

--- searchCourse/views.py
+++ searchCourse/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse
 from searchCourse.models import *
 
 def index(request):
-    subject_list = Subject.objects.order_by('letter_code')
-    course_list = Course.objects.order_by('name')[:20]
-    print(course_list)
-    context = {'subject_list':subject_list,
-                'course_list':course_list}
+    subject_list = Subject.objects.order_by("letter_code")
+    course_list = Course.objects.order_by("name")[:20]
+    #print(course_list)
+    context = {"subject_list":subject_list,
+                "course_list":course_list}
     return render(request,"searchCourse/index.html",context)
 
 # def subject(request, subject_id):
 #     return HttpResponse("You're looking at subject {}.".format(subject_id))
 
 def detail(request, subject_id, course_id):
-    return HttpResponse("You're looking at course {} {}.".format(subject_id,course_id))
+    subject_id = subject_id.upper()
+    select_subject = Subject.objects.get(letter_code=subject_id)
+    select_course = Course.objects.get(number_code=course_id,subject=select_subject.id)
+    #print(select_course)
+    context = {"course":select_course,
+                "subject":select_subject}
+    return render(request,"searchCourse/detail.html",context)
+    #return HttpResponse("You're looking at course {} {}.".format(subject_id,course_id))
 
 def rate(request, subject_id, course_id):
     return HttpResponse("You're rating the course {} {}.".format(subject_id,course_id))
