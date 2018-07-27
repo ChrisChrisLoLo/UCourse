c47967873e1807432bec3598b273a4fbfec465f6
Christian Lo <sporewoh@gmail.com>
2018-07-27 04:54:18 -0600

    SIngle course search form now works as intended.

--- searchCourse/static/searchCourse/js/search.js
+++ searchCourse/static/searchCourse/js/search.js
@@ -8,3 +8,18 @@ function searchSingle() {
     window.location.href = `/search/${subjectCode}/${courseCode}`;
     return;
 }
+
+//Code below suppresses typical form behavior for seaching for a single course, and makes the form
+//only run searchSingle() on submit. Did this so I can still use form validation.
+(function() {
+    "use strict";
+ 
+    window.addEventListener("load", function() {
+      document.getElementById("singleSearchForm").addEventListener("submit", function(event) {
+        event.target.checkValidity();
+        event.preventDefault(); // Prevent form submission and contact with server
+        event.stopPropagation();
+      }, false);
+    }, false);
+  }()
+);
