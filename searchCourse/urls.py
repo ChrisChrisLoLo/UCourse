3eab861efdc2b74de1c9487ea1b441a4ab50c569
Christian Lo <sporewoh@gmail.com>
2018-08-01 00:03:40 -0600

    Added pipfile, reorganized template directories, and edited templates

--- searchCourse/urls.py
+++ searchCourse/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'courseSearch'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='search:index'),
     #path('<str:subject_id>',views.subject, name='subject'),
-    path('<str:subject_id>/<int:course_id>',views.detail, name='detail'),
-    path('<str:subject_id>/<int:course_id>/rate',views.rate, name='rate'),
+    path('<str:subject_id>/<int:course_id>',views.detail, name='search:detail'),
+    path('<str:subject_id>/<int:course_id>/rate',views.rate, name='search:rate'),
 ]
