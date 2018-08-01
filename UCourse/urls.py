1aced232ab310f730430f71c2dfce2ab48056833
Christian Lo <sporewoh@gmail.com>
2018-08-01 00:34:15 -0600

    Created new app for the index page.  I'm assuming that the index should have views over simple templates to handle userAuth, but I could be wrong.

--- UCourse/urls.py
+++ UCourse/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
+    # path('/'),
     path('search/',include('searchCourse.urls')),
     #path('forums/',include('forums.urls')),
     path('admin/', admin.site.urls),
--- searchCourse/urls.py
+++ searchCourse/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'courseSearch'
 urlpatterns = [
-    path('', views.index, name='search:index'),
+    path('', views.index, name='courseSearch:index'),
     #path('<str:subject_id>',views.subject, name='subject'),
-    path('<str:subject_id>/<int:course_id>',views.detail, name='search:detail'),
-    path('<str:subject_id>/<int:course_id>/rate',views.rate, name='search:rate'),
+    path('<str:subject_id>/<int:course_id>',views.detail, name='courseSearch:detail'),
+    path('<str:subject_id>/<int:course_id>/rate',views.rate, name='courseSearch:rate'),
 ]
