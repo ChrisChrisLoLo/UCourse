c9aaacd1b65208d76b466400ee64f07779ac2d02
Christian Lo <sporewoh@gmail.com>
2018-08-01 00:49:54 -0600

    removed extraneous app, set up the index app. fixed namepace warnings.

--- UCourse/settings.py
+++ UCourse/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap4',
+    'index.apps.IndexConfig',
     'searchCourse.apps.SearchcourseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- searchCourse/urls.py
+++ searchCourse/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'courseSearch'
 urlpatterns = [
-    path('', views.index, name='courseSearch:index'),
+    path('', views.index, name='index'),
     #path('<str:subject_id>',views.subject, name='subject'),
-    path('<str:subject_id>/<int:course_id>',views.detail, name='courseSearch:detail'),
-    path('<str:subject_id>/<int:course_id>/rate',views.rate, name='courseSearch:rate'),
+    path('<str:subject_id>/<int:course_id>',views.detail, name='detail'),
+    path('<str:subject_id>/<int:course_id>/rate',views.rate, name='rate'),
 ]
