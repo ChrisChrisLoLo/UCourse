28d0f3dc1036f64a0b51d6ce22bdd495c00b74c6
Christian Lo <sporewoh@gmail.com>
2018-08-05 20:07:32 -0600

    SIgning in now works

---
+++ accounts/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from django.forms import widgets
+#from searchCourse.models import *
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-#from .forms import SignUpForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
 def sign_up(request):
-    form = 1
-    context = {
-                "form":form,
-            }
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+    context = {"form":form,}
     return render(request,"accounts/sign_up.html",context)
