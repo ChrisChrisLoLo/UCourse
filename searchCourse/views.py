19283985a08aec20e9e338a299564256a56d8992
Christian Lo <sporewoh@gmail.com>
2018-08-07 19:45:46 -0600

    "Optimized" course search and added the ability to sort by average score

--- searchCourse/views.py
+++ searchCourse/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.db.models import Avg
 
 from searchCourse.models import *
 from .forms import CourseForm, RatingForm
@@ -15,9 +16,6 @@ def index(request):
     form = CourseForm(DEF_SEARCH_PARAMS)
     search_params = form.data
 
-    #subject_list = Subject.objects.order_by("letter_code")
-    #course_list = Course.objects.order_by("name")
-
     if request.GET:
         get_form = CourseForm(request.GET)
         if get_form.is_valid():
@@ -28,9 +26,8 @@ def index(request):
         asc_char = "-"
     elif search_params["order"] == "ascending":
         asc_char = ""
-
-    search_courses = Course.objects.order_by(asc_char+search_params["sortBy"])
-    search_courses = search_courses.filter(
+    
+    search_courses = Course.objects.filter(
                     number_code__gte = int(search_params["courseMin"]),
                     number_code__lte = int(search_params["courseMax"]),
                     )
@@ -39,11 +36,22 @@ def index(request):
         search_courses = search_courses.filter(
                         subject__letter_code = search_params["subject"]
                         )
+
+    search_courses = search_courses.annotate(difficulty=Avg("rating__difficulty_score"))\
+                                    .annotate(workload=Avg("rating__workload_score"))\
+                                    .annotate(practicality=Avg("rating__practicality_score"))\
+                                    .annotate(enjoyment=Avg("rating__enjoyment_score"))\
+                                    .order_by(asc_char+search_params["sortBy"])
+    
+    #search_courses = Course.objects.order_by(asc_char+search_params["sortBy"])
+
+    
     
 
     search_paginator = Paginator(search_courses,15)
     page = request.GET.get("page")
     found_courses = search_paginator.get_page(page)
+
 
     context = {
                 "search_list":found_courses,
@@ -71,47 +79,49 @@ def detail(request, subject_id, course_id):
     #return HttpResponse("You're looking at course {} {}.".format(subject_id,course_id))
 
 def rate(request, subject_id, course_id):
-    if request.method == "POST":
-        form = RatingForm(request.POST)
-        if form.is_valid():
-            #validate subject/course id
-            subject_id = subject_id.upper()
-            select_subject = Subject.objects.get(letter_code=subject_id)
-            select_course = Course.objects.get(number_code=course_id,subject=select_subject.id)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = RatingForm(request.POST)
+            if form.is_valid():
+                #validate subject/course id
+                subject_id = subject_id.upper()
+                select_subject = Subject.objects.get(letter_code=subject_id)
+                select_course = Course.objects.get(number_code=course_id,subject=select_subject.id)
 
-            if select_course:
-                if not request.user.is_authenticated:
-                    return redirect("/accounts/login")
-                diff_score = form.cleaned_data.get("diffScore")
-                work_score = form.cleaned_data.get("workScore")
-                prac_score = form.cleaned_data.get("pracScore")
-                enjoy_score = form.cleaned_data.get("enjoyScore")
-                comment = form.cleaned_data.get("comment")
-                #Create rating form or update it if user has already created one.
-                #Rating is the object updated or created, created is a boolean 
-                rating, was_created = Rating.objects.update_or_create(
-                    course = select_course,
-                    user = request.user,
-                    defaults = {
-                        "difficulty_score":diff_score,
-                        "workload_score":work_score,
-                        "practicality_score":prac_score,
-                        "enjoyment_score":enjoy_score,
-                        "comment":comment
-                    }
-                )
-                return redirect("/search/success")
-            else:
-                #Raise error indicating no course found
-                pass
-            
-    else:
-        form = RatingForm()
+                if select_course:
+                    
+                    diff_score = form.cleaned_data.get("diffScore")
+                    work_score = form.cleaned_data.get("workScore")
+                    prac_score = form.cleaned_data.get("pracScore")
+                    enjoy_score = form.cleaned_data.get("enjoyScore")
+                    comment = form.cleaned_data.get("comment")
+                    #Create rating form or update it if user has already created one.
+                    #Rating is the object updated or created, created is a boolean 
+                    rating, was_created = Rating.objects.update_or_create(
+                        course = select_course,
+                        user = request.user,
+                        defaults = {
+                            "difficulty_score":diff_score,
+                            "workload_score":work_score,
+                            "practicality_score":prac_score,
+                            "enjoyment_score":enjoy_score,
+                            "comment":comment
+                        }
+                    )
+                    return redirect("/search/success")
+                else:
+                    #Raise error indicating no course found
+                    pass
+                
+        else:
+            form = RatingForm()
 
-    context = {"form":form,
-                "course_id":course_id,
-                "subject_id":subject_id}
-    return render(request,"searchCourse/rate_form.html",context)
+        context = {"form":form,
+                    "course_id":course_id,
+                    "subject_id":subject_id}
+        return render(request,"searchCourse/rate_form.html",context)
+    #Redirect if not authenticated
+    return redirect("/accounts/login")
 
 
 def success(request):
