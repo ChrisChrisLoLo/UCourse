72c015c4469c0060c9218c9958ec816d448f70a2
Christian Lo <sporewoh@gmail.com>
2018-08-17 16:49:43 -0600

    Added color coding to each criterion card

--- searchCourse/templatetags/searchCourseExtras.py
+++ searchCourse/templatetags/searchCourseExtras.py
@@ -19,3 +19,21 @@ def get_attribute(context, arg):
     if isinstance(attribute,float):
         attribute = Decimal(attribute).normalize()
     return attribute
+
+#Set color of the criterion card background and font based on the cards
+#score. This may be misuse of Bootstraps color palette.
+@register.simple_tag()
+def set_color(score):
+    textBSColor = "text-light"
+    if not score:
+        #if N/A
+        return textBSColor+" bg-secondary"
+    score = Decimal(score)
+    if score <= 1.7:
+        return textBSColor+" bg-danger"
+    elif score <= 3.3:
+        return textBSColor+" bg-warning"
+    else:
+        return textBSColor+" bg-success"
+        
+
